dc05b4ba96d9ceee74cf82772bd200368837dbdb
edgarlevyings44 <edgarlevy81@gmail.com>
2024-04-10 00:56:57 +0300

    first commit

---
+++ resources/views/note/edit.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <div class="note-container single-note">
+        <h1>Edit new note</h1>
+        <form action="{{ route('note.update', $note)}}" method= "post" class="note">
+            @csrf
+            @method('PUT')
+            <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here">
+                {{ $note->note }}
+            </textarea>
+            <div class="note-buttons">
+                <a href="{{ route('note.index')}}" class="note-cancle-button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
+</x-layout>
+
